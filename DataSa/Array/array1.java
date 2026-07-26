ce9b1d729aa856bdc40fcdcc98b315f0d1134f18
ctrlaltsave <gjanita9@gmail.com>
2026-07-26 10:54:23 +0530

    Array1

--- DataSa/Array/array1.java
+++ DataSa/Array/array1.java
@@ -1,6 +1,19 @@
-
-
+import java.util.*;
 public class array1 {
+  //implementing of array
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int [] numbers = new int[5];
+    System.out.println("Enter the numbers:");
+    for(int i=0;i<numbers.length;i++){
+      numbers[i]=sc.nextInt();
+    }
+    System.out.println("the numbers are");
+    for(int i=0;i<numbers.length;i++){
+      System.out.println(numbers[i]+" ");
+    }
+
+
   
-  
+  }
 }
