b4c3f3637d95c363a625877c0d93deded0dd19a2
ctrlaltsave <gjanita9@gmail.com>
2026-07-27 15:10:12 +0530

    Array7

---
+++ DataSa/Array/array6.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class array6 {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    //2d  dynamic array
+    List<List<Integer>> mat = new ArrayList<>();
+    for(int i=0;i<4;i++){
+      List<Integer> row = new ArrayList<>();
+      for(int j=0;j<3;j++){
+        int x=sc.nextInt();
+        row.add(x);
+      }
+      mat.add(row);
+    }
+    System.out.println(mat);
+
+
+  }
+  
+}
