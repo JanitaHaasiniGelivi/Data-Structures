7c855ac6ba68843e2261a79869abc5447b142ba4
ctrlaltsave <gjanita9@gmail.com>
2026-07-27 15:31:10 +0530

    Array8

---
+++ DataSa/Array/array7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class array7{
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    //array of objects
+    String [] arr= new String[4];
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.next(); 
+    }
+    System.out.println(Arrays.toString(arr));
+    arr[1]="haasini";
+    System.out.println(Arrays.toString(arr));
+  }
+}
