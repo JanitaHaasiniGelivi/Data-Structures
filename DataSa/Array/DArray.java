3a2ba41ce5c6438c4150cbd4fe7fb76557510e9e
ctrlaltsave <gjanita9@gmail.com>
2026-07-27 15:57:31 +0530

    file was added

---
+++ DataSa/Array/DArray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class DArray{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    //gnereal
+   // int[][] arr={
+    ///  {1,2,3},
+    //  {4,5,6},
+    //  {7,8,9}};
+    //  System.out.println(Arrays.toString(arr));
+      int[][] numbers={
+        {1,2,3},
+        {1},
+        {3,4,5}
+      };
+      System.out.println(Arrays.toString(numbers));
+      //taking input from the user
+     // int[][] ar=new int[3][2];
+      //for taking the input
+ //     for(int row=0;row<ar.length;row++){
+  //      for(int col=0;col<ar[row].length;col++){
+  //        arr[row][col]=sc.nextInt();        }
+   //   }
+      //for printing the output
+    //  for(int row=0;row<ar.length;row++){
+     //   for(int col=0;col<ar[row].length;col++){
+      //    System.out.println(Arrays.toString(ar[row]));
+     //   }
+      //}
+       for(int[] arr1:numbers){
+        System.out.println(Arrays.toString(arr1));
+       }
+  }
+}
